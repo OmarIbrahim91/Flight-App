fa7e337466db78c1008514dd382a4b4078ed0383
root <omarali43620@yahoo.com>
2020-02-10 12:29:13 +0100

    Updates

---
+++ precache-manifest.d052c6b81cc322c0e37fb65a893c21dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b49860c7f75461b0625e82eba9b0a629",
+    "url": "/Flight-App/index.html"
+  },
+  {
+    "revision": "398ad33458ad38ce57c6",
+    "url": "/Flight-App/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "d3d7a9d33c90b769b8c4",
+    "url": "/Flight-App/static/js/2.677bf8ce.chunk.js"
+  },
+  {
+    "revision": "398ad33458ad38ce57c6",
+    "url": "/Flight-App/static/js/main.a6504b9f.chunk.js"
+  },
+  {
+    "revision": "e040971c3996429622ce",
+    "url": "/Flight-App/static/js/runtime-main.f914e1bc.js"
+  }
+]);
